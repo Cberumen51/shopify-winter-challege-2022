e0051d69e1bc95828a0eb4081feffe7b54fcdb33
unknown <C.Berumen51@gmail.com>
2021-09-21 17:12:28 -0700

    got NASA api working wirht some framework

---
+++ spacestagram/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import NasaPictures from './components/picoftheday';
+
+function App() {
+  return (
+    <div className="App">
+      <NasaPictures />
+    </div>
+  );
+}
+
+export default App;
---
+++ spacestagram/src/components/picoftheday.js
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import {Card, CardImg, CardBody, CardTitle,CardText, CardSubtitle, Button } from 'reactstrap';
+
+
+
+export default function NasaPictures(props) {
+    const [picture, setPicture] = useState([])
+
+
+    useEffect(() => {
+        axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=FFcFHYCdovcMeEzuNxNwN21MVYGZU6xqlYAdUIZo')
+        .then(res => {
+            console.log(res)
+            setPicture(res.data)
+        })
+        .catch(err => {
+            console.log("There was an error, the data was not returend",err)
+        })
+    }, [])
+    console.log(picture)
+
+    return (
+        <div className="picture">
+            <Card>
+                <CardTitle>{picture.title}</CardTitle>
+                <CardSubtitle>{picture.copyright}</CardSubtitle>
+                <CardSubtitle>{picture.date}</CardSubtitle>
+                <CardBody>
+                <CardImg alt="NASA POTD" src={picture.url}/>
+                <CardText>{picture.explanation}</CardText>
+                </CardBody>
+                <Button>Like</Button>
+            </Card>
+        </div>
+    )
+}
